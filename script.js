91eb386a3193f2afae53178c7acfd1b685e35961
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-07-02 21:13:19 +0600

    shopping-cart

--- script.js
+++ script.js
@@ -2,70 +2,48 @@
 //--Plus Btn Event Handler--//
 const plusBtn = document.getElementById("plus-btn");
 plusBtn.addEventListener("click", function () {
-  shoppingCartPlusIcon("product-quantity", "product-price", 1219);
+  shoppingCartIconHandler("product-quantity", "product-price",1, 1219);
 });
 
 // --Minus Btn Event Handler --//
 const minusBtn = document.getElementById("minus-btn");
 minusBtn.addEventListener("click", function () {
-  shoppingCartMinusIcon("product-quantity", "product-price", 1219);
+  shoppingCartIconHandler("product-quantity", "product-price",-1, -1219);
 });
-
 
 
                     // -----iPhone Case ----//
 // --Plus Icon Event Handler --//
 const plusIcon = document.getElementById("plus-icon");
 plusIcon.addEventListener("click", function () {
-  shoppingCartPlusIcon("case-quantity", "case-price", 59);
+  shoppingCartIconHandler("case-quantity", "case-price",1, 59);
 });
 
 // --Minus Icon Event Handler --//
 const minusIcon = document.getElementById("minus-icon");
 minusIcon.addEventListener("click", function () {
-  shoppingCartMinusIcon("case-quantity", "case-price", 59);
+  shoppingCartIconHandler("case-quantity", "case-price",-1, -59);
+
 });
 
+                  // ----shopping Cart Icon Handler --//
+function shoppingCartIconHandler (id1,id2,count,perProductPrice){
+// --Quantity--//
+const quantity = document.getElementById(id1);
+const productQuantity = quantity.value;
+const productQuantityNumber = parseInt(productQuantity);
+const totalQuantity = productQuantityNumber + count;
+document.getElementById(id1).value = totalQuantity;
 
+//--Price --//
+const productPrice = document.getElementById(id2);
+const price = productPrice.innerText;
+const priceNumber = parseFloat(price);
+const productTotalPrice = priceNumber + perProductPrice;
+document.getElementById(id2).innerText = productTotalPrice;
 
-
-             // ----shoppingCartPlusIcon ----//
-function shoppingCartPlusIcon(id1, id2, perProductPrice) {
-  // --Quantity--//
-  const quantity = document.getElementById(id1);
-  const productQuantity = quantity.value;
-  const productQuantityNumber = parseInt(productQuantity);
-  const totalQuantity = productQuantityNumber + 1;
-  document.getElementById(id1).value = totalQuantity;
-
-  //--Price --//
-  const productPrice = document.getElementById(id2);
-  const price = productPrice.innerText;
-  const priceNumber = parseFloat(price);
-  const productTotalPrice = priceNumber + perProductPrice;
-  document.getElementById(id2).innerText = productTotalPrice;
-  calculateTotal();
-}
-
-
-
-             // ----shoppingCartMinusIcon ----//
-function shoppingCartMinusIcon(id1, id2, perProductPrice) {
-  // --Quantity--//
-  const quantity = document.getElementById(id1);
-  const productQuantity = quantity.value;
-  const productQuantityNumber = parseInt(productQuantity);
-  const totalQuantity = productQuantityNumber - 1;
-  document.getElementById(id1).value = totalQuantity;
-
-  //--Price --//
-  const productPrice = document.getElementById(id2);
-  const price = productPrice.innerText;
-  const priceNumber = parseFloat(price);
-  const productTotalPrice = priceNumber - perProductPrice;
-  document.getElementById(id2).innerText = productTotalPrice;
-  calculateTotal();
-}
+calculateTotal();
+}                 
 
 
 
